0ab0b6b7b4e73ad8dc8f096ac9364e3e36f644b7
khr5830@naver.com <37091602+burninghering@users.noreply.github.com>
2020-05-21 17:02:06 +0900

    아 진짜

---
+++ app/src/main/java/com/example/healthkeeperplz/ResultActivity.java
@@ -0,0 +1,94 @@
+package com.example.healthkeeperplz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    EditText weight, height; //BMI - 혜린
+    TextView resulttext;
+    String calculation, BMIresult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        weight = findViewById(R.id.weight); //BMI - 혜린
+        height = findViewById(R.id.height);
+        resulttext = findViewById(R.id.result);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu2:
+                Intent intent2 = new Intent(this, TrainerActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.menu3:
+                Intent intent3 = new Intent(this, InbodyActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.menu4:
+                Intent intent4 = new Intent(this, SettingActivity.class);
+                startActivity(intent4);
+                break;
+
+        }
+        return true;
+    }
+    public void calculateBMI(View view) { //BMI - 혜린
+        String S1 = weight.getText().toString();
+        String S2 = height.getText().toString();
+
+
+
+        float weightValue = Float.parseFloat(S1);
+        float heightValue = Float.parseFloat(S2) / 100;
+
+
+
+        float bmi = weightValue / (heightValue * heightValue);
+
+
+
+        if(bmi < 16){
+            BMIresult = "Severely Under Weight";
+        }else if(bmi < 18.5){
+            BMIresult = "Under Weight";
+        }else if(bmi >= 18.5 && bmi <= 24.9){
+            BMIresult = "Normal Weight";
+        }else if (bmi >= 25 && bmi <= 29.9){
+            BMIresult = "Overweight";
+        }else{
+            BMIresult = "Obese";
+        }
+
+
+
+        calculation = "Result:\n\n" + bmi + "\n" + BMIresult;
+        resulttext.setText(calculation);
+    }
+}
+
